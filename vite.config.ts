9217b7ef5510b033cb75ba296d768b546dd4a7b7
Kene Okoye <mr.kene.okoye@gmail.com>
2023-05-31 20:05:52 +0200

    build: :wrench: AT-01: Add dependency and configurations required to be able create and use path alias in TypeScript imports with Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    /*
+     * Maps the @ path alias to the src directory using
+     * the path.resolve method to get the full file path
+     */
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
