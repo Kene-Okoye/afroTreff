7f89ebdcbfe1f22da5609377e2e1d612c82660ee
Kene Okoye <mr.kene.okoye@gmail.com>
2023-07-14 11:38:35 +0200

    style: :lipstick: AT-07:  Include the recently created LinkButton to the component and also add the styles for the news and story categories.

--- src/components/cardBlog/CardBlog.tsx
+++ src/components/cardBlog/CardBlog.tsx
@@ -1,5 +1,5 @@
 import cardBlogStyles from '@/components/cardBlog/CardBlog.module.css';
-import Button from '@/components/button/Button';
+import LinkButton from '@/components/linkButton/LinkButton';
 
 type CardBlogProps = {
   heading: string;
@@ -8,6 +8,7 @@ type CardBlogProps = {
   imageSrc?: string;
   imageAlt?: string;
   buttonText: string;
+  href: string;
 };
 
 const CardBlog = ({
@@ -17,22 +18,37 @@ const CardBlog = ({
   imageSrc,
   imageAlt,
   buttonText,
+  href,
 }: CardBlogProps) => {
   return (
     <>
       <div className={cardBlogStyles['blog-item']}>
-        <div className={cardBlogStyles['blog-item-image-wrapper']}>
+        <div className={cardBlogStyles['blog-item--image-wrapper']}>
           {/* //TODO: Reintergrate after pics with the perfect dimension are ontained from Francis */}
-          <img src={imageSrc} alt={imageAlt} className={cardBlogStyles['blog-item-image']} />
+          <img src={imageSrc} alt={imageAlt} className={cardBlogStyles['blog-item--image']} />
         </div>
-        <div className={cardBlogStyles['blog-item-text-wrapper']}>
-          <div className={cardBlogStyles['blog-item-header-category-wrapper']}>
-            <h2 className={cardBlogStyles['blog-item-heading']}>{heading}</h2>
-            <p className={cardBlogStyles['blog-item-category']}>{category}</p>
+        <div
+          className={`${cardBlogStyles['blog-item--text-wrapper']} ${
+            category === 'news' && cardBlogStyles['blog-item--text-wrapper-news']
+          }`}
+        >
+          <div className={cardBlogStyles['blog-item--header-category-wrapper']}>
+            <h2 className={cardBlogStyles['blog-item--heading']}>{heading}</h2>
+            <p
+              className={`${cardBlogStyles['blog-item--category']} ${
+                category === 'news' && cardBlogStyles['blog-item--category-news']
+              }`}
+            >
+              {category}
+            </p>
           </div>
-          <div className={cardBlogStyles['blog-item-text-button-wrapper']}>
-            <p className={cardBlogStyles['blog-item-text']}>{textContent}</p>
-            <Button text={buttonText} />
+          <div
+            className={`${cardBlogStyles['blog-item--text-button-wrapper']} ${
+              category === 'story' && cardBlogStyles['blog-item--text-button-wrapper-story']
+            }`}
+          >
+            <p className={cardBlogStyles['blog-item--text']}>{textContent}</p>
+            <LinkButton linkText={buttonText} href={href} />
           </div>
         </div>
       </div>
