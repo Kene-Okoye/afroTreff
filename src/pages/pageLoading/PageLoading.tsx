ab9548b2aff453017a96884d0fbceb9ca8c9f87f
Kene Okoye <mr.kene.okoye@gmail.com>
2023-07-31 13:24:14 +0200

    style: :lipstick: AT-19: Add loading line for the page not found and adjust PageNotFound text size.

--- src/pages/pageLoading/PageLoading.tsx
+++ src/pages/pageLoading/PageLoading.tsx
@@ -3,7 +3,10 @@ import pageLoadingStyles from '@/pages/pageLoading/PageLoading.module.css';
 const PageLoading = () => {
   return (
     <div className={pageLoadingStyles['page-loading']}>
-      <p>page is loading</p>
+      <div className={pageLoadingStyles['page-loading--container']}>
+        <p>Page is loading</p>
+        <div className={pageLoadingStyles['page-loading--loading-line']}></div>
+      </div>
     </div>
   );
 };
