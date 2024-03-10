3af6b6f1e5b5d7ef95f0b5e51ba9c639eb58e04c
Kene Okoye <mr.kene.okoye@gmail.com>
2024-03-11 00:03:25 +0100

    feat: :package: AT-22: Add i18next to the project to be used for client side translations.

---
+++ src/i18n/config.ts
@@ -0,0 +1,33 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  // Add React bindings as a plugin.
+  .use(HttpApi)
+  .use(initReactI18next)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  .init({
+    // Config options
+    fallbackLng: 'en',
+
+    // Enables useful output in the browser’s
+    // dev console.
+    debug: true,
+
+    // Normally, we want `escapeValue: true` as it
+    // ensures that i18next escapes any code in
+    // translation messages, safeguarding against
+    // XSS (cross-site scripting) attacks. However,
+    // React does this escaping itself, so we turn
+    // it off in i18next.
+    // => https://www.i18next.com/translation-function/interpolation#unescape
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
