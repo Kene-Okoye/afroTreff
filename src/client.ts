624913885e386e1982ca597b58eb125790ebc259
Kene Okoye <mr.kene.okoye@gmail.com>
2024-03-12 13:27:39 +0100

    feat: :alien: AT-33: Use the new sanity ProjectId in the React project client config file.

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,7 @@
 import { createClient, type ClientConfig } from '@sanity/client';
 
 const config: ClientConfig = {
-  projectId: '5ir4p4o5',
+  projectId: 'hc828wun',
   dataset: 'production',
   useCdn: true, // set to `false` to bypass the edge cache
   apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
