c016196e3f4b4e3c8e3c74a01160e16f4de8ce58
Kene Okoye <mr.kene.okoye@gmail.com>
2024-03-15 13:19:34 +0100

    build: :building_construction: DEPENDENCIES UPGRADE: Update vite and other associated configs.

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
+  base: '/',
   resolve: {
     /*
      * Maps the @ path alias to the src directory using
