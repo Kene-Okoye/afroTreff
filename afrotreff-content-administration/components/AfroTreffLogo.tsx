8a2c9d0fcf3b96e06e10af484afb42d7f2febed4
Kene Okoye <mr.kene.okoye@gmail.com>
2024-03-19 15:27:35 +0100

    feat: :sparkles: AT-33: Implement custom themes for the Sanity.io studio - add AfroTreff logo and color for focus and hover .

---
+++ afrotreff-content-administration/components/AfroTreffLogo.tsx
@@ -0,0 +1,13 @@
+const AfroTreffLogo = () => {
+  return (
+    <>
+      <img
+        src="../assets/afrotreff_logo_cropped.png"
+        alt="Afrotreff logo"
+        style={{height: '100%', width: '100%'}}
+      />
+    </>
+  )
+}
+
+export default AfroTreffLogo
--- afrotreff-content-administration/sanity.config.ts
+++ afrotreff-content-administration/sanity.config.ts
@@ -5,9 +5,18 @@ import {schemaTypes} from './schemaTypes'
 import {deskStructure} from './deskStructure'
 import {media} from 'sanity-plugin-media'
 
+import AfroTreffLogo from './components/AfroTreffLogo'
+
+const {theme} = (await import(
+  // @ts-expect-error -- TODO setup themer.d.ts to get correct typings
+  'https://themer.sanity.build/api/hues?primary=793101'
+)) as {theme: import('sanity').StudioTheme}
+
 export default defineConfig({
   name: 'default',
   title: 'afrotreff-content-administration',
+  icon: AfroTreffLogo,
+  theme: theme,
 
   projectId: 'hc828wun',
   dataset: 'production',
